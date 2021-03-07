5b89a2d173bfc08f278c485f600235531fccbb7d
Diago Monte <51008626+Cubas93@users.noreply.github.com>
2021-03-07 15:47:03 -0500

    Update CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
-import Fab from "@material-ui/icons/Fab";
+import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
